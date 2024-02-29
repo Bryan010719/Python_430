462624aee0df9f34068762842315f8c337915938
Bryan Isaac Lucatero Cervantes <126211404+Bryan010719@users.noreply.github.com>
2024-02-29 12:49:25 -0600

    Add files via upload

---
+++ Python/00_HelloWorld.py
@@ -0,0 +1,9 @@
+#este simbolo se utiliza para comentar.
+print("Hola grupo 430 de la carrera tecnica de programacion")
+#como consultar el tipo de dato
+print(type("soy un dato str")) #tipo"str"
+print(type(5)) #tipo"int"
+print(type(1.5)) #tipo"float"
+print(type(3 + 1j)) #tipo"complex"
+print(type(True)) #tipo"bool"
+print(type(print("mi cadena de texto"))) #tipo"NoneType"
